e7d2c8191ff3c1e16e760426ec1e40ca719e72c7
Jonghwa Woo <wjh0630@gmail.com>
2014-11-27 02:07:53 +0900

    Added user_update

--- problems/crawl/setup.py
+++ problems/crawl/setup.py
@@ -5,4 +5,3 @@ import django
 sys.path.append('/home/raison/www/algospot')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "algospot.settings")
 django.setup()
-
--- problems/crawl/update.py
+++ problems/crawl/update.py
@@ -1,1 +1,40 @@
+import os
+import json
 import setup
+from problems.models import Problem, User
+
+
+def update_problem():
+    pass
+
+def update_user():
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    user_dir = os.path.join(cur_dir, 'json/user')
+
+    user_files = [os.path.join(user_dir, x) for x in os.listdir(user_dir)]
+
+    for user_file in user_files:
+        with open(user_file, 'r') as f:
+            user = json.loads(f.read())[0]
+
+            uid = user['uid'][0].strip()
+            name = user['name'][0].strip()
+
+            try:
+                u = User.objects.get(name=name)
+            except:
+                u = User(uid=uid, name=name)
+                u.save()
+
+            for p in user['problems']:
+                keyword = p.strip()
+
+                try:
+                    p = Problem.objects.get(keyword=keyword)
+                except:
+                    continue
+                u.problem.add(p)
+
+if __name__ == '__main__':
+    update_problem()
+    update_user()
