588383dd9a684908d6a38b7650819189991badf2
Jonghwa Woo <wjh0630@gmail.com>
2014-11-27 02:05:41 +0900

    Added a file containing tracking user ids

--- problems/crawl/crawl.py
+++ problems/crawl/crawl.py
@@ -1,4 +1,5 @@
 import os
+import json
 from glob import glob
 from subprocess import call, check_call
 
@@ -8,7 +9,10 @@ try:
 except:
     pass
 
+with open('users','r') as f:
+    uids = json.loads(f.read())
+
 os.chdir("algo-crawl")
 call(["scrapy", "crawl", "-o", "../json/problem.json", "problem"])
-for uid in [9324,11033,36,764,2,1337,200]:
+for uid in uids:
     call(["scrapy", "crawl", "-o", "../json/user/%d.json"%uid, "-a", "uid=%d"%uid, "user"])
