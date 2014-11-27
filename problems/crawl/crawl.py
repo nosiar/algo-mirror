9e50e53ab993956752240879ca8966b79e9e328e
Jonghwa Woo <wjh0630@gmail.com>
2014-11-27 10:09:21 +0900

    Changed to absolute paths

--- problems/crawl/crawl.py
+++ problems/crawl/crawl.py
@@ -3,16 +3,22 @@ import json
 from glob import glob
 from subprocess import call, check_call
 
+cur_dir = os.path.dirname(os.path.realpath(__file__))
+json_dir = os.path.join(cur_dir, 'json')
+user_dir = os.path.join(json_dir, 'user')
+
 try:
-    check_call(['rm'] + glob(os.path.join('json/', '*.json')))
-    check_call(['rm'] + glob(os.path.join('json/user/', '*.json')))
+    check_call(['rm'] + glob(os.path.join(json_dir, '*.json')))
+    check_call(['rm'] + glob(os.path.join(user_dir, '*.json')))
 except:
     pass
 
-with open('users','r') as f:
+with open(os.path.join(cur_dir, 'users'),'r') as f:
     uids = json.loads(f.read())
 
-os.chdir("algo-crawl")
-call(["scrapy", "crawl", "-o", "../json/problem.json", "problem"])
+os.chdir(os.path.join(cur_dir, 'algo-crawl'))
+call(['scrapy', 'crawl', '-o',
+      os.path.join(json_dir, 'problem.json'), 'problem'])
 for uid in uids:
-    call(["scrapy", "crawl", "-o", "../json/user/%d.json"%uid, "-a", "uid=%d"%uid, "user"])
+    call(['scrapy', 'crawl', '-o',
+          os.path.join(user_dir, '%d.json'%uid), '-a', 'uid=%d'%uid, 'user'])
--- problems/crawl/update.sh
+++ problems/crawl/update.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-python crawl.py
-/bin/bash -c ". ~/py3env/bin/activate; exec python update.py"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+python $DIR/crawl.py
+/bin/bash -c ". ~/py3env/bin/activate; exec python $DIR/update.py"
 
