cf21198d5ff8078fe286933356c1409562282569
Jonghwa Woo <wjh0630@gmail.com>
2017-01-31 00:08:57 +0900

    Use venv

--- problems/crawl/update.sh
+++ problems/crawl/update.sh
@@ -3,5 +3,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 python $DIR/crawl.py
-/bin/bash -c ". ~/py3env/bin/activate; exec python $DIR/update.py"
+/bin/bash -c ". ~/www/algospot/venv/bin/activate; exec python $DIR/update.py"
 
