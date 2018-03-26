388fb2061e3579ce03f17d7a56e69478d7ec1ae1
Jonghwa Woo <wjh0630@gmail.com>
2018-03-27 01:47:05 +0900

    Django 2.0

--- problems/urls.py
+++ problems/urls.py
@@ -3,6 +3,6 @@ from problems import views
 
 urlpatterns = [
     re_path(r'^$', views.IndexView.as_view(), name='index'),
-    re_path(r'^data/$', views.data, name='data'),
-    re_path(r'^user/(?P<name>\w+)/$', views.user, name='user'),
+    re_path(r'^data/?$', views.data, name='data'),
+    re_path(r'^user/(?P<name>\w+)/?$', views.user, name='user'),
 ]
